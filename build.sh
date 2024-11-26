5a118b2c145ec92f2eaed266b6890f125e7af666
Shahram Haj mohammadi <Shahram4wd@yahoo.com>
2024-11-25 23:26:16 -0800

    python not find. changed the python version to 3.11

--- build.sh
+++ build.sh
@@ -2,11 +2,18 @@
 # Exit on error
 set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
+# Create and activate a virtual environment
+python -m venv /opt/render/project/.venv
+source /opt/render/project/.venv/bin/activate
+
+# Upgrade pip to the latest version
+pip install --upgrade pip
+
+# Install dependencies
 pip install -r requirements.txt
 
-# Convert static asset files
+# Collect static files
 python manage.py collectstatic --no-input
 
-# Apply any outstanding database migrations
+# Apply database migrations
 python manage.py migrate
