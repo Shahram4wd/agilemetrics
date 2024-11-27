f4ee9e7d310bead00a6de190212bef53dd4ee098
Shahram Haj mohammadi <Shahram4wd@yahoo.com>
2024-11-26 23:48:24 -0800

    I do not know any more

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 # exit on error
+#!/bin/bash
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+
 set -o errexit
 python -m pip install --upgrade pip
 
