69053f7247636952f463f36ab4226fbc47b26eb0
Shahram Haj mohammadi <Shahram4wd@yahoo.com>
2024-11-30 01:51:12 -0800

    added data_import command and celery beat

--- agilemetrics/settings.py
+++ agilemetrics/settings.py
@@ -47,6 +47,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	
+    'data_import',
+	'django_celery_beat',
 ]
 
 MIDDLEWARE = [
